2e51a7be9f9d7bb432b55fa1f346865e3d2fd5cf
Punam-Nikam <punamnikam.835@gmail.com>
2026-05-11 22:37:58 +0530

    Added new Java files

---
+++ GradingStudent.java
@@ -0,0 +1,60 @@
+//Problem : 8 (Grading Students)
+
+import java.util.*;
+
+public class GradingStudent {
+
+    // Function to round grades
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int grade : grades) {
+
+            // No rounding if grade is less than 38
+            if (grade < 38) {
+                result.add(grade);
+            } 
+            else {
+
+                // Find next multiple of 5
+                int nextMultiple = ((grade / 5) + 1) * 5;
+
+                // Round if difference is less than 3
+                if (nextMultiple - grade < 3) {
+                    result.add(nextMultiple);
+                } 
+                else {
+                    result.add(grade);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Number of students
+        int n = sc.nextInt();
+
+        List<Integer> grades = new ArrayList<>();
+
+        // Input grades
+        for (int i = 0; i < n; i++) {
+            grades.add(sc.nextInt());
+        }
+
+        // Call function
+        List<Integer> result = gradingStudents(grades);
+
+        // Print output
+        for (int grade : result) {
+            System.out.println(grade);
+        }
+
+        sc.close();
+    }
+}
