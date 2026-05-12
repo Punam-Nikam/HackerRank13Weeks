74d511edae0d11dc93509a9275863a5bafd12ff8
Punam-Nikam <punamnikam.835@gmail.com>
2026-05-12 22:14:54 +0530

    updated code and  optimize code structure

--- CountingSort.java
+++ CountingSort.java
@@ -9,12 +9,10 @@ public class CountingSort {
 
         List<Integer> freq = new ArrayList<>();
 
-        // Initialize frequency list with 100 zeros
         for (int i = 0; i < 100; i++) {
             freq.add(0);
         }
 
-        // Count frequencies
         for (int num : arr) {
             freq.set(num, freq.get(num) + 1);
         }
@@ -23,26 +21,18 @@ public class CountingSort {
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
-        // Number of elements
         int n = sc.nextInt();
 
         List<Integer> arr = new ArrayList<>();
 
-        // Input elements
         for (int i = 0; i < n; i++) {
             arr.add(sc.nextInt());
         }
-
         List<Integer> result = countingSort(arr);
-
-        // Print frequency array
         for (int x : result) {
             System.out.print(x + " ");
         }
-
         sc.close();
     }
 }
