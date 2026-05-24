7017fb2bf8a92c1dd309957b7dd37a7f0df18dff
Punam-Nikam <punamnikam.835@gmail.com>
2026-05-24 19:04:17 +0530

    Adding new programs

---
+++ LeftRotationArray.java
@@ -0,0 +1,33 @@
+//PROBLEM - 27 lEFT ROTATION ARRAY
+//Rotate(Shift) the array to the left by d positions, where each left rotation shifts every element one position toward the beginning of the array.
+// Return the final array after completing all rotations.
+
+import java.util.*;
+public class LeftRotationArray 
+{
+    public static void main(String[] args) {
+        
+        // ArrayList<Integer> list = new ArrayList<>(
+        //         Arrays.asList(10, 20, 30, 40, 50));
+        
+        Scanner sc=new Scanner(System.in);
+        List <Integer> list =new ArrayList<>();
+
+        System.out.println("Enter shift count : ");
+        int d=sc.nextInt();
+        System.out.println("Enter how many nos : ");
+        int n=sc.nextInt();
+
+        System.out.println("Enter numbers : ");
+        for(int i=0;i<n;i++){
+                int c =sc.nextInt();
+                list.add(c);
+
+        }
+
+        Collections.rotate(list, d);
+
+        System.out.println(list);
+    }
+}
+        
