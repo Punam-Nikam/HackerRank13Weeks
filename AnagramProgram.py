aea34a591f2da55daf8ce0958beb85cee0981521
Punam-Nikam <punamnikam.835@gmail.com>
2026-05-29 12:36:28 +0530

    Adding new code

--- AnagramProgram.py
+++ AnagramProgram.py
@@ -1,31 +1,28 @@
-# import math
-# import os
-# import random
-# import re
-# import sys
-# from collections import Counter
+# # WEEK TEST - 4
+# Given a string, divide it into two equal halves. Determine the minimum number of character changes required in the first half to make it an anagram of the second half. If the string length is odd, return -1.
+
+from collections import Counter
+
+def anagram(s):
+
+    n = len(s)
+
+    if n % 2 != 0:
+        return -1
+
+    s1 = s[:n//2]
+    s2 = s[n//2:]
+
+    count1 = Counter(s1)
+    count2 = Counter(s2)
+
+    diff = count1 - count2
+
+    return sum(diff.values())
 
 
-# def anagram(s):
+q = int(input())
 
-#     n=len(s);
-#     if n%2 !=0:
-#         return -1
-    
-#     s1=s[:n//2]
-#     s2=s[n//2:]
-    
-#     count1=Counter(s1)
-#     count2=Counter(s2)
-    
-#     diff=count1-count2
-    
-#     return sum(diff.values())
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-#     q = int(input().strip())
-#     for q_itr in range(q):
-#         s = input()
-#         result = anagram(s)
-#         fptr.write(str(result) + '\n')
-#     fptr.close()
+for _ in range(q):
+    s = input()
+    print(anagram(s))
