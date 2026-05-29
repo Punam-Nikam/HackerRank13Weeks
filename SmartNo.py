cdc9b8f4623a00e3aeba1cd3ad2fa476d0958b4a
Punam-Nikam <punamnikam.835@gmail.com>
2026-05-29 12:31:43 +0530

    Adding new code

--- SmartNo.py
+++ SmartNo.py
@@ -8,7 +8,7 @@ import math
 
 def is_smart_number(num):
     val = int(math.sqrt(num))
-    if num / val == val:
+    if val*val == num:          #num / val == val:
         return True
     return False
 
